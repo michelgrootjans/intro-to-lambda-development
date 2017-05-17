91ab0356137c692ac93b4e9dde637325554ce98f
Michel Grootjans <michel.grootjans@gmail.com>
2017-05-17 10:25:12 +0200

    added dynamic path handler

--- tasks/web-api/api.js
+++ tasks/web-api/api.js
@@ -6,3 +6,13 @@ module.exports = api;
 api.get('/hello', function (request) {
 	return 'hello world';
 });
+
+api.get('/greet', function(request) {
+	const name = request.queryString.name;
+	return 'hello ' + name;
+});
+
+api.get('/greet/{name}', function(request) {
+	const name = request.pathParams.name;
+	return 'hello again, ' + name;
+});
